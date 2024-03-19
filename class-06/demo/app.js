aca916562719f791434544f9c221ab8c94bed23c
Admin Enter Tech School <138706221+entertechschool-admin@users.noreply.github.com>
2024-03-18 22:02:00 -0500

    Create app.js

---
+++ class-06/demo/app.js
@@ -0,0 +1,38 @@
+let miEdad = 100;
+console.log("Soy JS y estoy dentro de HTML");
+// alert(miEdad);
+// document.write("SPORTING CRISTAL");
+
+// let usuarioNombre = prompt("Cual es tu nombre?");
+
+// console.log("Bienvenido " + usuarioNombre);
+// document.write("Bienvenido " + usuarioNombre);
+
+// let x = 5;
+// let y = 6;
+
+// if (x == y) {
+// 	document.write("X es igual a Y");
+// } else {
+// 	document.write("X NO es igual a Y");
+// }
+
+let horaActual = 10;
+
+// if (horaActual > 17) {
+// 	document.write("Buenas noches querido alumno");
+// } else {
+// 	if (horaActual > 12) {
+// 		document.write("Buenas tardes querido alumno");
+// 	} else {
+// 		document.write("Buenos dias querido alumno");
+// 	}
+// }
+
+if (horaActual > 17 && a > b) {
+	document.write("Buenas noches querido alumno");
+} else if (horaActual > 12) {
+	document.write("Buenas tardes querido alumno");
+} else {
+	document.write("Buenos dias querido alumno");
+}
